83dde93534da09100706a54414b04b1bff3548c1
JoeSemper <joe.semper@gmail.com>
2024-02-14 16:22:58 +0300

    Initial commit

---
+++ app/src/main/java/com/joesemper/rtspcamera/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.joesemper.rtspcamera
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.joesemper.rtspcamera.ui.theme.RtspCameraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RtspCameraTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Stream")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val uri = remember {
+        Uri.parse("rtsp://b03773d78e34.entrypoint.cloud.wowza.com:1935/app-4065XT4Z/80c76e59_stream1")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        AndroidView(
+            modifier = Modifier.size(200.dp),
+            factory = {context ->
+            com.alexvas.rtsp.widget.RtspSurfaceView(context).apply {
+                init(uri, null, null)
+                start(requestVideo = true, requestAudio = false)
+            }
+        })
+    }
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RtspCameraTheme {
+        Greeting("Android")
+    }
+}
