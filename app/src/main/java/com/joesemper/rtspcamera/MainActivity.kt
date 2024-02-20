9ee1b07dae684de56bc10677fbdb5979742682ba
JoeSemper <joe.semper77@gmail.com>
2024-02-20 12:53:52 +0300

    Interface improvement

--- app/src/main/java/com/joesemper/rtspcamera/MainActivity.kt
+++ app/src/main/java/com/joesemper/rtspcamera/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.joesemper.rtspcamera
 
-import android.net.Uri
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -29,48 +20,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RtspCameraTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Greeting("Stream")
                     AppNavHost()
                 }
             }
         }
+
+        setFullscreen()
+        setScreenAlwaysOn()
+
+    }
+
+    private fun setFullscreen() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
-
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val uri = remember {
-        Uri.parse("rtsp://b03773d78e34.entrypoint.cloud.wowza.com:1935/app-4065XT4Z/80c76e59_stream1")
-    }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        AndroidView(
-            modifier = Modifier.size(200.dp),
-            factory = {context ->
-            com.alexvas.rtsp.widget.RtspSurfaceView(context).apply {
-                init(uri, null, null)
-                start(requestVideo = true, requestAudio = false)
-            }
-        })
     }
 
-
-
+    private fun setScreenAlwaysOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
 }
--- app/src/main/java/com/joesemper/rtspcamera/ui/screens/stream/StreamScreen.kt
+++ app/src/main/java/com/joesemper/rtspcamera/ui/screens/stream/StreamScreen.kt
@@ -1,25 +1,28 @@
 package com.joesemper.rtspcamera.ui.screens.stream
 
+import android.content.res.Configuration
 import android.net.Uri
-import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,9 +31,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.alexvas.rtsp.widget.RtspSurfaceView
+import kotlinx.coroutines.delay
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -42,6 +48,29 @@ fun StreamScreen(
     val uri = remember(viewModel.uiState) { Uri.parse(viewModel.uiState) }
 
     var status by remember { mutableStateOf("") }
+
+    var showLayout by remember { mutableStateOf(true) }
+
+    val context = LocalContext.current
+
+    val configuration = LocalConfiguration.current
+
+    val videoViewModifier = when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = true)
+                .border(width = 0.1.dp, color = Color.Red)
+        }
+
+        else -> {
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = false)
+                .border(width = 0.1.dp, color = Color.Red)
+        }
+    }
+
 
     val listener = object : RtspSurfaceView.RtspStatusListener {
         override fun onRtspStatusConnecting() {
@@ -75,29 +104,44 @@ fun StreamScreen(
         }
     }
 
-    if (viewModel.uiState.isNotEmpty()) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            AndroidView(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1f),
-                factory = { context ->
-                    RtspSurfaceView(context).apply {
-                        init(uri, null, null)
-                        setStatusListener(listener)
-                        start(requestVideo = true, requestAudio = false)
-                    }
+
+    Box(
+        modifier = Modifier.fillMaxSize().background(color = Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        AndroidView(
+            modifier = videoViewModifier,
+            factory = { context ->
+                RtspSurfaceView(context).apply {
+                    init(uri, null, null)
+                    setStatusListener(listener)
+                    start(requestVideo = true, requestAudio = false)
                 }
-            )
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.TopStart
+            }
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable {
+                    showLayout = true
+                },
+            contentAlignment = Alignment.TopStart
+        ) {
+            AnimatedVisibility(
+                visible = showLayout,
+                enter = fadeIn(),
+                exit = fadeOut()
             ) {
+
+                LaunchedEffect(key1 = context) {
+                    delay(5000)
+                    showLayout = false
+                }
+
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
@@ -121,6 +165,7 @@ fun StreamScreen(
             }
 
         }
+
     }
 
 }
