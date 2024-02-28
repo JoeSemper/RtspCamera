562b581ea0575ae346bc7146ed8cffa2d7794d88
JoeSemper <joe.semper77@gmail.com>
2024-02-28 16:46:46 +0300

    Added Stream screen log

--- app/src/main/java/com/joesemper/rtspcamera/ui/screens/stream/StreamScreen.kt
+++ app/src/main/java/com/joesemper/rtspcamera/ui/screens/stream/StreamScreen.kt
@@ -10,22 +10,35 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,153 +48,199 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.alexvas.rtsp.widget.RtspSurfaceView
 import com.joesemper.rtspcamera.R
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StreamScreen(
     navigateHome: () -> Unit,
     viewModel: StreamViewModel = getViewModel()
 ) {
 
-    val state = viewModel.uiState
-
-    var status = viewModel.uiState.status.getString()
+    val state = viewModel.uiState.collectAsStateWithLifecycle().value
+    val status = state.status.getString()
 
     var showLayout by remember { mutableStateOf(true) }
 
     val context = LocalContext.current
-
     val configuration = LocalConfiguration.current
-
-    val videoViewModifier = remember(configuration.orientation) {
-        when (configuration.orientation) {
-            Configuration.ORIENTATION_LANDSCAPE -> {
-                Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = true)
-                    .border(width = 0.1.dp, color = Color.Red)
-            }
-
-            else -> {
-                Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = false)
-                    .border(width = 0.1.dp, color = Color.Red)
-            }
-        }
-    }
-
 
     val listener = object : RtspSurfaceView.RtspStatusListener {
         override fun onRtspStatusConnecting() {
             super.onRtspStatusConnecting()
-            viewModel.onConnecting()
+            viewModel.updateStatus(StreamStatus.Connecting)
         }
 
         override fun onRtspStatusDisconnected() {
             super.onRtspStatusDisconnected()
-            viewModel.onError(context.getString(R.string.disconnected))
+            viewModel.updateStatus(StreamStatus.Disconnected)
         }
 
         override fun onRtspStatusFailedUnauthorized() {
             super.onRtspStatusFailedUnauthorized()
-            viewModel.onError(context.getString(R.string.unauthorized))
+            viewModel.updateStatus(StreamStatus.Error(context.getString(R.string.unauthorized)))
         }
 
         override fun onRtspStatusFailed(message: String?) {
             super.onRtspStatusFailed(message)
-            viewModel.onError(message ?: "")
+            viewModel.updateStatus(StreamStatus.Error(message ?: ""))
         }
 
         override fun onRtspStatusConnected() {
             super.onRtspStatusConnected()
-            viewModel.onConnected()
-        }
-
-        override fun onRtspFirstFrameRendered() {
-            super.onRtspFirstFrameRendered()
-            status = "First frame rendered"
+            viewModel.updateStatus(StreamStatus.Connected)
         }
 
         override fun onRtspStatusDisconnecting() {
             super.onRtspStatusDisconnecting()
-            status = "Disconnecting"
+            viewModel.updateStatus(StreamStatus.Disconnecting)
         }
     }
 
+    val streamView: MutableState<RtspSurfaceView?> = remember(context) { mutableStateOf(null) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.Black),
-        contentAlignment = Alignment.Center
-    ) {
-        if (state.status != StreamStatus.Loading) {
+    DisposableEffect(key1 = state.currentUri, key2 = streamView) {
 
-            val uri = remember(viewModel.uiState) { Uri.parse(state.currentUri) }
-
-            AndroidView(
-                modifier = videoViewModifier,
-                factory = { context ->
-                    RtspSurfaceView(context).apply {
-                        init(uri, null, null)
-                        setStatusListener(listener)
-                        start(requestVideo = true, requestAudio = false)
-                    }
-                }
-            )
+        if (state.currentUri.isNotEmpty()) {
+            streamView.value?.apply {
+                val uri = Uri.parse(state.currentUri)
+                init(uri, null, null)
+                setStatusListener(listener)
+                start(requestVideo = true, requestAudio = false)
+            }
         }
 
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .clickable {
-                    showLayout = true
-                },
-            contentAlignment = Alignment.TopStart
-        ) {
-            AnimatedVisibility(
-                visible = showLayout,
-                enter = fadeIn(),
-                exit = fadeOut()
-            ) {
+        onDispose {
+            streamView.value?.stop()
+        }
+    }
 
-                LaunchedEffect(key1 = context) {
-                    delay(5000)
-                    showLayout = false
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { paddingValues ->
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    IconButton(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Default.Clear, contentDescription = null)
+                    }
                 }
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    Text(
-                        modifier = Modifier.alpha(0.4f),
-                        text = status,
-                        color = Color.White
-                    )
-
-                    IconButton(
-                        modifier = Modifier.alpha(0.4f),
-                        colors = IconButtonDefaults.iconButtonColors(contentColor = Color.White),
-                        onClick = { navigateHome() }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Settings,
-                            contentDescription = null
-                        )
+                    items(state.statusLog.size) {
+                        Text(text = state.statusLog[it].getString())
                     }
                 }
             }
-
         }
 
+
+        Box(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .background(color = Color.Black),
+            contentAlignment = Alignment.Center
+        ) {
+
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(
+                        ratio = 1f,
+                        matchHeightConstraintsFirst = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+                    )
+                    .border(
+                        width = 0.1.dp,
+                        color = Color.Red
+                    ),
+                factory = { context ->
+                    RtspSurfaceView(context).apply { streamView.value = this }
+                }
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        showLayout = true
+                    },
+                contentAlignment = Alignment.TopStart
+            ) {
+                AnimatedVisibility(
+                    visible = showLayout,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+
+                    LaunchedEffect(key1 = context) {
+                        delay(5000)
+                        showLayout = false
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        TextButton(
+                            modifier = Modifier.alpha(0.8f),
+                            onClick = { showBottomSheet = true },
+                        ) {
+                            Text(
+                                color = Color.White,
+                                text = status
+                            )
+                        }
+
+                        IconButton(
+                            modifier = Modifier.alpha(0.8f),
+                            colors = IconButtonDefaults.iconButtonColors(contentColor = Color.White),
+                            onClick = { navigateHome() }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Settings,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                }
+
+            }
+
+        }
     }
+
 
 }
--- app/src/main/java/com/joesemper/rtspcamera/ui/screens/stream/StreamViewModel.kt
+++ app/src/main/java/com/joesemper/rtspcamera/ui/screens/stream/StreamViewModel.kt
@@ -2,20 +2,26 @@ package com.joesemper.rtspcamera.ui.screens.stream
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.joesemper.rtspcamera.R
 import com.joesemper.rtspcamera.data.datastore.RtspDataStore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.util.Calendar
 
 class StreamViewModel(
     private val datastore: RtspDataStore
 ) : ViewModel() {
-    var uiState by mutableStateOf(StreamScreenState())
-        private set
+
+    private val _uiState = MutableStateFlow(StreamScreenState())
+    val uiState = _uiState.asStateFlow()
 
     init {
         loadData()
@@ -24,41 +30,47 @@ class StreamViewModel(
     private fun loadData() {
         viewModelScope.launch {
             datastore.getCurrentStreamUri().collect { uri ->
-                uiState = if (uri.isEmpty()) {
-                    uiState.copy(status = StreamStatus.Error("Empty Uri"))
+                _uiState.value = if (uri.isEmpty()) {
+                    _uiState.value.copy(
+                        statusLog = _uiState.value.statusLog.plus(StreamStatus.Error("Empty Uri"))
+                    )
                 } else {
-                    uiState.copy(currentUri = uri, status = StreamStatus.Connecting)
+                    _uiState.value.copy(
+                        currentUri = uri,
+                        statusLog = _uiState.value.statusLog.plus(StreamStatus.Ready)
+                    )
                 }
             }
         }
     }
 
-    fun onConnecting() {
-        uiState = uiState.copy(status = StreamStatus.Connecting)
-    }
-
-    fun onConnected() {
-        uiState = uiState.copy(status = StreamStatus.Connected)
-    }
-
-    fun onError(massage: String) {
-        uiState = uiState.copy(status = StreamStatus.Error(massage))
+    fun updateStatus(newStatus: StreamStatus) {
+        _uiState.value = _uiState.value.copy(statusLog = _uiState.value.statusLog.plus(newStatus))
     }
 
 }
 
 data class StreamScreenState(
     val currentUri: String = "",
-    val status: StreamStatus = StreamStatus.Loading
-)
+    val statusLog: List<StreamStatus> = listOf(StreamStatus.Loading),
+) {
+    val status: StreamStatus
+        get() = statusLog.maxByOrNull { it.date }!!
+}
 
 sealed class StreamStatus {
     @Composable
     abstract fun getString(): String
+    val date = Calendar.getInstance().time.time
 
     object Loading : StreamStatus() {
         @Composable
         override fun getString(): String = stringResource(R.string.loading)
+    }
+
+    object Ready : StreamStatus() {
+        @Composable
+        override fun getString(): String = stringResource(R.string.ready_to_connect)
     }
 
     class Error(val massage: String = "") : StreamStatus() {
@@ -69,6 +81,16 @@ sealed class StreamStatus {
     object Connecting : StreamStatus() {
         @Composable
         override fun getString(): String = stringResource(R.string.connecting)
+    }
+
+    object Disconnected : StreamStatus() {
+        @Composable
+        override fun getString(): String = stringResource(id = R.string.disconnected)
+    }
+
+    object Disconnecting : StreamStatus() {
+        @Composable
+        override fun getString(): String = stringResource(R.string.disconnecting)
     }
 
     object Connected : StreamStatus() {
